681c0b5899770d5c3d7f7946e287dbbc345c8425
travaditaher <travaditaher8@gmail.com>
2024-01-03 20:38:12 -0800

    custom hook for fetching games

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
-import GameGrid from "./components/gameGrid";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
