5110ce0f7e5091611504d538ccf656664c3e0c9f
travaditaher <travaditaher8@gmail.com>
2024-01-04 13:10:40 -0800

    built searching element

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,17 @@
 import { HStack, Image, space } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
+import SearchInput from './SearchInput'
 
+interface SearchInputProps{
+  onSearch: (searchText: string) => void;
+}
 
-const NavBar = () => {
+const NavBar = ({onSearch}:SearchInputProps) => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
+    <HStack padding='10px'>
         <Image src={logo} boxSize='60px'></Image>
+        <SearchInput onSearch ={onSearch} />
         <ColorModeSwitch />
     </HStack>
   )
