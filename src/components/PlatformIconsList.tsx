62cafddbba9189b4d3776d2ee2722f4f3201b306
travaditaher <travaditaher8@gmail.com>
2024-01-03 21:14:27 -0800

    displaying playform icons

---
+++ src/components/PlatformIconsList.tsx
@@ -0,0 +1,41 @@
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface PlatformIconsListProps {
+  platforms: Platform[];
+}
+
+const PlatformIconsList = ({ platforms }: PlatformIconsListProps) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <HStack marginY={'10px'}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='gray.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconsList;
